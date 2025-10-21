b8eca50a5e98bce8a0f0ae5e295d5f598e31079d
wilsonz3-dev <177430156+wilsonz347@users.noreply.github.com>
2025-10-20 23:26:05 -0400

    Added function to restructure output into md file

--- src/report_generator.py
+++ src/report_generator.py
@@ -1,3 +1,23 @@
 """
 MARKDOWN REPORT GENERATION
 """
+from pathlib import Path
+
+def generate_report(summaries, output_dir='reports/', filename='report.md'):
+  """
+  Restructure report in markdown format
+  
+  Returns: Path to generated report
+  """
+  # Generate a new folder
+  Path(output_dir).mkdir(parents=True, exist_ok=True)
+  rpath = Path(output_dir) / filename
+  
+  # Write to file: structure it as markdown
+  with open(rpath, "w") as f:
+    f.write("# Codebase Summary Report\n\n")
+    for item in summaries:
+      f.write(f"## {item['File']}\n")
+      f.write(f"{item['Summary']}\n\n")
+      
+  return str(rpath)
